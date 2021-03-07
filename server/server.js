486064508e1ef472c0d79d01238e7f6a34c86c65
nafizw <nafiz@mztech.us>
2021-03-07 18:58:49 +0600

    port change

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
