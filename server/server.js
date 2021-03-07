d94cb5602fab56ee423a926d66c6db2adfbfe6d7
nayeem-abdullah-mz <nayeem@mztech.us>
2021-03-07 19:21:37 +0600

    test

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const app = express();
 dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+    res.send("API is running... hi");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
